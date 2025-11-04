ca9a4e4ba4070377e6a1ab0f085651388ac9cbf2
sinamardanime <marsy127@mymail.unisa.edu.au>
2025-11-04 12:39:16 +1030

    added a basic animal class

--- animal.py
+++ animal.py
@@ -5,4 +5,30 @@ Author: Billy Bizilis
 ID: 110100110
 Username: bizvy001
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+
+
+class Animal:
+    """
+    Base class for all animals in the zoo.
+    Each animal has a name, species, age, and dietary needs.
+    """
+    def __init__(self, name, species, age, diet):
+        self.name = name
+        self.species = species
+        self.age = age
+        self.diet = diet
+
+    # Common behaviours shared by all animals
+    def make_sound(self):
+        return self.name + " makes a generic animal sound."
+
+    def eat(self):
+        return self.name + " is eating " + self.diet + "."
+
+    def sleep(self):
+        return self.name + " is sleeping peacefully."
+
+    def __str__(self):
+        return self.species + " named " + self.name + ", Age: " + str(self.age) + ", Diet: " + self.diet
+
