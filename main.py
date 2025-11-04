ed9e60c55bdd900e592dd93178bdff4c44f8bfe4
sinamardanime <marsy127@mymail.unisa.edu.au>
2025-11-04 13:34:43 +1030

    made a bird.py file with basic attributes

--- main.py
+++ main.py
@@ -9,19 +9,7 @@ This is my own work as defined by the University's Academic Integrity Policy.
 
 from animal import Animal
 from mammal import Mammal
+from bird import Bird
 
-
-def main():
-    print("=== MAMMAL CLASS DEMONSTRATION ===\n")
-    print("Creating a mammal with full details:\n")
-    lion = Mammal("Leo", "Lion", 7, "Meat", "Thick", "Golden", "Savannah", 80, 190)
-    print(lion)
-    print(lion.make_sound())
-    print(lion.feed_young())
-    print(lion.move())
-    print()
-
-if __name__ == "__main__":
-    main()
 
 
