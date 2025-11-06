111e1c3863e7ea077e349b3d7424a265ec499e1a
sinamardanime <marsy127@mymail.unisa.edu.au>
2025-11-06 14:26:09 +1030

    added an init method and started with the enclosure

--- enclosure.py
+++ enclosure.py
@@ -5,4 +5,15 @@ Author: Billy Bizilis
 ID: 110100110
 Username: bizvy001
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+
+
+
+class Enclosure:
+    """
+    The Enclosure class represents a specific area within the zoo that houses
+    animals of a compatible type and environment. It tracks size, environment type,
+    cleanliness level, and the animals it contains.
+    """
+
+    def __init__(self, name, environment_type, size=100, cleanliness=100):
