03d5d9f14a9c5cd61503f89919fe73bba146e5b5
sinamardanime <marsy127@mymail.unisa.edu.au>
2025-11-06 14:27:09 +1030

    added getter and setter for getting the name of the specific enclosure

--- enclosure.py
+++ enclosure.py
@@ -17,3 +17,17 @@ class Enclosure:
     """
 
     def __init__(self, name, environment_type, size=100, cleanliness=100):
+
+    pass
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, new_name):
+        if isinstance(new_name, str) and len(new_name.strip()) > 0:
+            self.__name = new_name
+        else:
+            print("Invalid name. Defaulting to 'Unnamed Enclosure'.")
+            self.__name = "Unnamed Enclosure"
+
+    name = property(get_name, set_name)
