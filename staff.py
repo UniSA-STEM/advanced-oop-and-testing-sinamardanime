1bea8f18869fe7882504d912927c0a4787a14e11
sinamardanime <marsy127@mymail.unisa.edu.au>
2025-11-06 13:34:52 +1030

    created an init for staff

--- staff.py
+++ staff.py
@@ -5,4 +5,21 @@ Author: Billy Bizilis
 ID: 110100110
 Username: bizvy001
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+
+# staff.py
+from abc import ABC, abstractmethod
+
+class Staff(ABC):
+    """
+    The Staff class is an abstract base class representing all staff members in the zoo.
+    It defines common attributes and enforces the implementation of specific methods
+    (e.g., perform_duty) in subclasses.
+    """
+
+    def __init__(self, name, role):
+        self.__name = name
+        self.__role = role
+        self.__assigned_animals = []
+        self.__assigned_enclosures = []
+
