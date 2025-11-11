af991d20904a300a7de678a215dfb8d425d0f73c
sinamardanime <marsy127@mymail.unisa.edu.au>
2025-11-11 15:02:08 +1030

    added methods

--- reptile.py
+++ reptile.py
@@ -34,3 +34,28 @@ class Reptile(Animal):
         self.scale_type = scale_type
         self.habitat = habitat
         self.is_venomous = is_venomous
+        # === Methods ===
+
+    def make_sound(self):
+        """Override make_sound for reptiles."""
+        return self.get_name() + " hisses softly."
+
+    def move(self):
+        """Describe reptile movement."""
+        return self.get_name() + " slithers or crawls across the " + self.habitat.lower() + "."
+
+    def shed_skin(self):
+        """Unique reptile behaviour."""
+        return self.get_name() + " is shedding its old skin to grow a new layer."
+
+    def __str__(self):
+        """Return a detailed description of the reptile."""
+        venom_status = "venomous" if self.is_venomous else "non-venomous"
+        return (
+                self.get_name() + " is a " + self.get_species() +
+                " that lives in the " + self.habitat.lower() + ". " +
+                "It has " + self.scale_type.lower() + " scales, is " + venom_status +
+                ", and maintains a body temperature around " + str(self.temperature) + "°C. " +
+                self.get_name() + " follows a " + self.diet.lower() +
+                " diet and is currently " + str(self.age) + " years old."
+        )
